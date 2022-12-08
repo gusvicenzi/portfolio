8e527ca27b3dc0c4c56ef10a29563a4f4eb1bc12
Gustavo Vicenzi <gusvicenzi12@gmail.com>
2022-12-08 18:26:46 -0300

    Create about page. Minor fixes in other components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.scss'
 import { Navbar } from './components/navbar/Navbar'
+import { About } from './pages/about/About'
 import { Home } from './pages/home/Home'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar items={['Home', 'About', 'Skills', 'Education', 'Portfolio', 'Get in touch']} />
       <Home />
+      <About />
     </div>
   )
 }
---
+++ src/pages/about/About.tsx
@@ -0,0 +1,10 @@
+import './about.scss'
+
+export function About() {
+  return (
+    <section id="about">
+      <h1>&lt;About me /&gt;</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam asperiores repellendus blanditiis officiis nulla, optio neque expedita excepturi commodi enim numquam dolore ab consectetur porro, hic consequatur? Accusamus, vitae est!</p>
+    </section>
+  )
+}
